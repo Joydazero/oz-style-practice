2b1864c4a11540d30df6c3b20c52fe9348e6b3a8
chodayoung <verysweetkey@gmail.com>
2025-08-20 14:44:31 +0900

    11일차 과제 완료

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,47 @@
+import styled from "styled-components";
+
 export default function Content({ content }) {
+  const CategoryIcon = styled.span`
+    font-size: 12px;
+    font-weight: 400;
+    color: ${ ({ now }) =>   now === "모집중" ? "#d7fa00" : "#cbccca" };
+    border: ${ ({ now }) =>   now === "모집중" ? "1px solid #d7fa00" : "1px solid #cbccca" };
+    padding: 4px 5px;
+    border-radius: 3px;
+    `
+
+    const ContentTitle = styled.div`
+    font-size: 18px;
+    font-weight: 600;
+    `
+    
+    const ContentSubTitle = styled.p`
+    font-size: 12px;
+    font-weight: 400;
+    color: rgb(160, 160, 160);
+    `
+
+    const ContentContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: flex-start;
+    gap: 5px;
+    flex-wrap: nowrap;
+    `
+
+    const ContentImg = styled.img`
+      width: 300px;
+      border-radius: 10px;
+      margin-bottom: 3px;
+      min-height: 225px;
+    `
   return (
-    <div className="content-container">
-      <img src={content.img} alt={content.title} />
-      <span>모집중</span>
-      <div>{content.title}</div>
-      <p>{content.subtitle}</p>
-    </div>
+    <ContentContainer>
+      <ContentImg src={content.img} alt={content.title}></ContentImg>
+      <CategoryIcon  now={content.now} >{content.now}</CategoryIcon>
+      <ContentTitle>{content.title}</ContentTitle>
+      <ContentSubTitle>{content.subtitle}</ContentSubTitle>
+    </ContentContainer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,45 @@
+import styled from "styled-components";
+
 export default function Header() {
+  const HeaderContainer = styled.header`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    gap: 0;
+    flex-wrap: nowrap;
+    padding: 20px 20px;
+    background-color: black;
+  `
+
+  const MenuContainer = styled.ul`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    flex-wrap: nowrap;
+  `
+  const MenuContainerLi = styled.li`
+    font-size: 16px;
+    font-weight: 400;
+    list-style: none;
+    `
+  const HeaderTitle = styled.h1`
+    font-size:32px;
+    font-weight:700;  
+    @media (max-width: 768px) {
+      font-size:26px;
+    }
+  `
   return (
-    <header className="header-container">
-      <h1>OZ코딩스쿨</h1>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
-      </ul>
-    </header>
+    <HeaderContainer className="header-container">
+      <HeaderTitle>OZ코딩스쿨</HeaderTitle>
+      <MenuContainer>
+        <MenuContainerLi>로그인</MenuContainerLi>
+        <MenuContainerLi>회원가입</MenuContainerLi>
+        <MenuContainerLi>내클래스</MenuContainerLi>
+      </MenuContainer>
+    </HeaderContainer>
   );
 }
